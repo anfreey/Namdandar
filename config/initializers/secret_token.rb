b325348b442308489e63ca82a97695e17e804ee5
anfreey <anfreey@gmail.com>
2012-12-18 21:15:00 +0400

    first comment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Namdandar::Application.config.secret_token = 'cc65781d017bf44c2e9949256756ef39481fa1d0fc24b463a1db8b64921e17bbdc1c9f4991660a58110136e346cb30300ea9cd52c8834111edaa77ab5db50e6f'
